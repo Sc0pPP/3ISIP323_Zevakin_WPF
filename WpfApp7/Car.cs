e25532ea6ecf91fc1e561978d3822c9901a1f19c
Sc0pPP <Zevakidana08@gmail.com>
2025-12-10 15:34:16 +0300

    написал cs код для первой страницы engine

--- WpfApp7/Car.cs
+++ WpfApp7/Car.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfApp7
 {
-    internal class Car
+    public enum engine
     {
+        N50B30=30000,
+        N63S63=35000,
+        M30B30=15000,
+        S54B32=20000,
     }
+     public enum marka
+    {
+        m5e60=1000000,
+        m5f10=800000,
+        m4g82=1300000,
+        m3g80=1200000
+    }
+    public enum color
+    {
+        gold=300000,
+        black=150000,
+        white=300000,
+        pink=100000
+    }
+    public enum options
+    {
+        stove=50000,
+        windows=100000,
+        steering_wheel=70000,
+        seat_vmesto_bytilky=150000
+    }
+   
+    internal static class Car
+    {
+        public static marka marka {  get;private set; }
+        public static engine engine { get; private set; }
+        public static color color { get; private set; }
+        public static options options { get; private set; }
+
+
+    }
+    
 }
--- WpfApp7/pages/Engine.xaml.cs
+++ WpfApp7/pages/Engine.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,29 +16,42 @@ using System.Windows.Shapes;
 
 namespace WpfApp7.pages
 {
-    /// <summary>
-    /// Логика взаимодействия для Engine.xaml
-    /// </summary>
+   
     public partial class Engine : Page
     {
+        
+        /// <summary>
+        /// Логика взаимодействия для Engine.xaml
+        /// </summary>
+        public static List<engine> engin = Enum.GetValues(typeof(engine))
+                                              .Cast<engine>() // Приведение типов
+                                              .ToList(); // Преобразование в List
+        public static List<marka> mark= Enum.GetValues(typeof(marka))
+                                              .Cast<marka>() // Приведение типов
+                                              .ToList(); // Преобразование в List
         public Engine()
         {
             InitializeComponent();
+            type_engine.ItemsSource = engin;
+            type_car.ItemsSource = mark;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            NavigationService.Navigate(new ColorDop());
         }
 
         private void type_car_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            if (type_car.SelectedItem != null)
+            {
+                marka selectedMark = (marka)Enum.Parse(typeof(marka), type_car.SelectedItem.ToString());
+            }
         }
 
         private void type_engine_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            engine selectedMark = (engine)Enum.Parse(typeof(engine), type_engine.SelectedItem.ToString());
         }
     }
 }
