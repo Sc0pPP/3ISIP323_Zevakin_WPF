d80b6180aed36fe4d82c8e99ae5d4c5a6c2e8ffb
Sc0pPP <Zevakidana08@gmail.com>
2025-12-10 13:50:28 +0300

    написал xaml для первой страницы

--- WpfApp7/MainWindow.xaml.cs
+++ WpfApp7/MainWindow.xaml.cs
@@ -24,5 +24,22 @@ namespace WpfApp7
         {
             InitializeComponent();
         }
+
+        private void BackButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                MainFrame.GoBack();
+            }
+        }
+        private void MainFrame_OnNavigated(object sender, NavigationEventArgs e)
+        {
+            BackButton.Visibility = MainFrame.CanGoBack ? Visibility.Visible : Visibility.Collapsed;
+
+            if (e.Content is Page page)
+            {
+                TitleTextBlock.Text = page.Title;
+            }
+        }
     }
 }
---
+++ WpfApp7/pages/Engine.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Engine.xaml
+    /// </summary>
+    public partial class Engine : Page
+    {
+        public Engine()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void type_car_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void type_engine_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
